7916b587808cb9189541092d1b115d34fd0eb16f
manuelpezzi <pezzimanuel@gmail.com>
2025-01-28 13:17:02 +0100

    ciclo for creato

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ function fetchEmails() {
     for (let i = 0; i < 10; i++) {
         fetch('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(response => response, json())
+            .then(data => {
+                const emailItem = document.createElement('li');
+                emailItem.className = 'list-group-item';
+                emailItem.textContent = data.response;
+                emailList.appendChild(emailItem);
+            })
 
-
+            .catch(error => console.error(error))
     }
 }
+
 /*Descrizione
 Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 Bonus
